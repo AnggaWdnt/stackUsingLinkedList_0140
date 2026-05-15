c8de9a890dbc91e757e578eabda041036a891e05
AnggaWdnt <widiangga94@gmail.com>
2026-05-15 16:41:48 +0700

    menambahkan fitur exit program

--- stack.cpp
+++ stack.cpp
@@ -103,6 +103,10 @@ int main()
             case 3:
             stack.peek();
             break;
+
+            case 4:
+            cout << "Exiting program." << endl;
+            break;
         }
     }
 }
